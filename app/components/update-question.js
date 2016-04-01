aa926a7e417d96876bcad8aeb6cd540992532d46
Haley <hebristol@mgmail.com>
2016-04-01 11:35:02 -0700

    add functional new question component

---
+++ app/components/update-question.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    updateQuestionForm() {
+      this.set('updateQuestionForm', true);
+    },
+    update(question) {
+      var params = {
+        content: this.get('content'),
+        author: this.get('author'),
+        notes: this.get('notes')
+      };
+      this.set('updateQuestionForm', false);
+      this.sendAction('update', question, params);
+    }
+  }
+});
