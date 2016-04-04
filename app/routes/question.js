6b5377fa0436837e11ee0ac55eb45a71371264ab
Haley <hebristol@gmail.com>
2016-04-04 15:16:06 -0700

    add functional answer component

--- app/routes/question.js
+++ app/routes/question.js
@@ -12,7 +12,7 @@ export default Ember.Route.extend({
       newAnswer.save().then(function(){
         return question.save();
       });
-      this.transitionTo('index');
+      this.transitionTo('question');
     }
   }
 });
