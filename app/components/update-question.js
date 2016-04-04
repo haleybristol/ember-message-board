5acfd2bee177cb7e9d3251f190f395ae0120c35e
haley bristol <hebristol@gmail.com>
2016-04-03 19:28:32 -0700

    add functional update button

--- app/components/update-question.js
+++ app/components/update-question.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateQuestionForm: false,
   actions: {
     updateQuestionForm() {
       this.set('updateQuestionForm', true);
@@ -9,7 +10,7 @@ export default Ember.Component.extend({
       var params = {
         content: this.get('content'),
         author: this.get('author'),
-        notes: this.get('notes')
+        notes: this.get('notes'),
       };
       this.set('updateQuestionForm', false);
       this.sendAction('update', question, params);
