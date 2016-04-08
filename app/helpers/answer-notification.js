485c5be178281c65ce53e5cec548ca4cb6bf33f1
Haley <hebristol@gmail.com>
2016-04-08 16:54:59 -0700

    edit custom helper display number of answers to each question

--- app/helpers/answer-notification.js
+++ app/helpers/answer-notification.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export function answerNotification(params/*, hash*/) {
   var question = params[0];
 
-  if(question.get('answers').get('length') == 0) {
-    return Ember.String.htmlSafe('<br><span class="glyphicon glyphicon-star"></span>No Answer');
+  if(question.get('answers').get('length') >= 0) {
+    return Ember.String.htmlSafe('<br><br><span class="">' + question.get('answers').get('length') + ' Answer(s)</span><br>');
   }
 }
 
