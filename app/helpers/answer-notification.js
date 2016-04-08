cc06e5d622474ad169faf677252a1fbab01473a9
Haley <hebristol@gmail.com>
2016-04-08 16:50:08 -0700

    add custom answer helper

--- app/helpers/answer-notification.js
+++ app/helpers/answer-notification.js
@@ -3,8 +3,8 @@ import Ember from 'ember';
 export function answerNotification(params/*, hash*/) {
   var question = params[0];
 
-  if(question.get('answers').get('length') >= 0) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-star-empty">Not Been Answered</span>');
+  if(question.get('answers').get('length') == 0) {
+    return Ember.String.htmlSafe('<br><span class="glyphicon glyphicon-star"></span>No Answer');
   }
 }
 
